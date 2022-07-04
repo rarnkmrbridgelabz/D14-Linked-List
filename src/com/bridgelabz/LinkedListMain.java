29769a5c0c2fc1ddcb3132fdd27afe9f7b69205e
Arun <rarnkmrbridgelabz.com>
2022-07-05 05:07:58 +0530

    Adding UC-3

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -3,12 +3,12 @@ package com.bridgelabz;
 public class LinkedListMain {
 	public static void main(String[] args) {
 		LinkedList linkedlist = new LinkedList();
-		
-		linkedlist.addFirst(70);
+		System.out.println("LINKED LIST");
+		linkedlist.addLast(56);
 		linkedlist.display();
-		linkedlist.addFirst(30);
+		linkedlist.addLast(30);
 		linkedlist.display();
-		linkedlist.addFirst(56);
+		linkedlist.addLast(70);
 		linkedlist.display();
 	}
 }
